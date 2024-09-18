e2dfea09f801e2596f77424215002e257cb9e0c8
Caymaar <119420644+Caymaar@users.noreply.github.com>
2024-09-18 23:17:33 +0200

    No more array, auto generative class

--- Classes/Model.py
+++ Classes/Model.py
@@ -13,7 +13,7 @@ class BlackScholesModel(Market, Option):
         Market.__init__(self, S0, rate, sigma)
         Option.__init__(self, K, T, option_type, option_style)
 
-    def calculate_option_price(self):
+    def option_price(self):
         d1 = (np.log(self.S0 / self.K) + (self.rate + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * np.sqrt(self.T))
         d2 = d1 - self.sigma * np.sqrt(self.T)
 
@@ -25,10 +25,13 @@ class BlackScholesModel(Market, Option):
         return option_price
     
 class TrinomialTreeModel(Market, Option):
+    
     def __init__(self, S0, rate, sigma, K, T, option_type, option_style, N, dividend=0.0, ex_div_date=None):
         Market.__init__(self, S0, rate, sigma, dividend, ex_div_date)
         Option.__init__(self, K, T, option_type, option_style)
         self.N = N
+        self.root = Node(S0, 0, self)
+        self.root.cum_prob = 1.0
 
     def get_deltaT(self, factor=0):
         exp_factors = np.exp(np.linspace(0, factor, self.N))  # Exponential scaling factors
@@ -39,132 +42,131 @@ class TrinomialTreeModel(Market, Option):
             return self.dividend
         return 0.0
 
-    def compute_probability(self, node, step):
-        deltaT = self.deltaT_array[step]
-        alpha = math.exp(self.sigma * math.sqrt(3 * deltaT))
+    def build_tree(self, factor=0, threshold=0):
 
-        esp = node.value * math.exp(self.rate * deltaT) - self.dividend_value(step)
-        var = node.value ** (2) * math.exp(2 * self.rate * deltaT) * (math.exp(self.sigma ** 2 * deltaT) - 1)
+        if self.root.forward_mid_neighbor is not None:
+            self.root = Node(self.S0, 0, self)
+            self.root.cum_prob = 1.0
 
-        forward = node.value * math.exp(self.rate * deltaT) - self.dividend_value(step)
-        p_d = (forward ** (-2) * (var + esp ** 2) - 1 - (alpha + 1) * (esp / forward - 1)) / ((1 - alpha) * (alpha ** (-2) - 1))
-        p_u = (esp / forward - 1 - (1 / alpha - 1) * p_d) / (alpha - 1)
-        p_m = 1 - p_u - p_d
-
-        return p_d, p_u, p_m
-    
-    def compute_values(self, node, step):
-        deltaT = self.deltaT_array[step]
-        alpha = math.exp(self.sigma * math.sqrt(3 * deltaT))
-
-        u_value = (node.value * math.exp(self.rate * deltaT) - self.dividend_value(step)) * alpha
-        d_value = (node.value * math.exp(self.rate * deltaT) - self.dividend_value(step)) / alpha
-        m_value = node.value * math.exp(self.rate * deltaT) - self.dividend_value(step)
-
-        return d_value, u_value, m_value
-    
-    def build_tree(self, factor=0):
-
+        self.threshold = threshold
+            
         self.get_deltaT(factor)
         self.dividend_step = math.ceil(self.ex_div_date * self.N)
 
-        # Initialisation de l'arbre trinomial
-        self.tree = [[None for _ in range(2 * N + 1)] for N in range(self.N + 1)]
+        trunc = self.root
 
-        # Définir la racine de l'arbre (niveau 0)
-        self.tree[0][0] = Node(self.S0, prob=1.0, cum_prob=1.0)
+        for _ in range(0, self.N, 1): 
 
-        # Construire l'arbre trinomial
-        for step in range(self.N):  # Parcours de chaque niveau
+            trunc.create_forward_nodes()
 
-            for i in range(2 * step + 1):  # Parcours de chaque nœud au niveau
+            trunc.generate_upper_neighbors()
+            trunc.generate_lower_neighbors()
+
+            trunc = trunc.forward_mid_neighbor
                 
-                if self.tree[step][i] is not None:  # Si le nœud existe
+    def compute_payoff_and_return_last_trunc(self):
 
-                    current_node = self.tree[step][i]
+        trunc = self.root
 
-                    # Calcul des probabilités pour chaque direction
-                    p_d, p_u, p_m = self.compute_probability(current_node, step)
+        while trunc.forward_mid_neighbor is not None:
+            trunc = trunc.forward_mid_neighbor
 
-                    d_forward, u_forward, m_forward = self.compute_values(current_node, step)
+        last_node = trunc
 
-                    # DOWN
-                    if self.tree[step + 1][i] is None:
-                        down_node = Node(d_forward, prob=p_d, cum_prob=p_d * current_node.cum_prob)
-                        self.tree[step + 1][i] = down_node
-                    else:
-                        down_node = self.tree[step + 1][i]
-                        down_node.cum_prob += p_d * current_node.cum_prob
+        trunc.payoff()
 
-                    # MID
-                    if self.tree[step + 1][i + 1] is None:
-                        mid_node = Node(m_forward, prob=p_m, cum_prob=p_m * current_node.cum_prob)                        
-                        self.tree[step + 1][i + 1] = mid_node
-                    else:
-                        mid_node = self.tree[step + 1][i + 1]
-                        mid_node.cum_prob += p_m * current_node.cum_prob
-                        mid_node.value = m_forward
-                
-                    # UP
-                    if self.tree[step + 1][i + 2] is None:
-                        up_node = Node(u_forward, prob=p_u, cum_prob=p_u * current_node.cum_prob)
-                        self.tree[step + 1][i + 2] = up_node
-                    else:
-                        up_node = self.tree[step + 1][i + 2]
-                        up_node.cum_prob += p_u * current_node.cum_prob
+        while last_node.up_neighbor is not None:
+            last_node = last_node.up_neighbor
+            last_node.payoff()
 
-                    # Lier les enfants au parent
-                    current_node.children = [(down_node, p_d), (mid_node, p_m), (up_node, p_u)]
+        last_node = trunc
 
-        # Calculer les valeurs terminales de l'option
-        for i in range(2 * self.N + 1):
-            self.tree[self.N][i].payoff(self.K, self.option_type)
+        while last_node.down_neighbor is not None:
+            last_node = last_node.down_neighbor
+            last_node.payoff()
+
+        return trunc
+    
+    def back_propagation_pricing(self, last_trunc):
+
+        while last_trunc.backward_neighbor is not None:
+            last_trunc = last_trunc.backward_neighbor
+            last_trunc.compute_option_price()
+
+            node_to_compute = last_trunc
+
+            while node_to_compute.up_neighbor is not None:
+                node_to_compute = node_to_compute.up_neighbor
+                node_to_compute.compute_option_price()
+
+            node_to_compute = last_trunc
+
+            while node_to_compute.down_neighbor is not None:
+                node_to_compute = node_to_compute.down_neighbor
+                node_to_compute.compute_option_price()
 
     def calculate_option_price(self):
-        """
-        Calculate the option price using backward induction for both European and American styles.
-        """
-        # Calcul du facteur d'actualisation
-        discount = np.exp(-self.rate * (self.T / self.N))
-        
-        # Parcours inverse de l'arbre (induction inverse)
-        for j in range(self.N - 1, -1, -1):
-            for i in range(2 * j + 1):
-                current_node = self.tree[j][i]
+
+        last_trunc = self.compute_payoff_and_return_last_trunc()
+
+        self.back_propagation_pricing(last_trunc)
+
+        return self.root.option_price
+    
+    def save_tree_to_dataframe(self):
+ 
+        max_columns = self.N + 1 
+        max_rows = 2 * self.N + 1 
+        extra_rows = int(math.ceil(self.N * 0.05))
+        df1 = pd.DataFrame(index=range(max_rows + extra_rows), columns=range(max_columns))
+        df2 = pd.DataFrame(index=range(max_rows + extra_rows), columns=range(max_columns))
+        df3 = pd.DataFrame(index=range(max_rows + extra_rows), columns=range(max_columns))
+
+        trunc = self.root
+
+        for step in range(0, self.N + 1):
             
-                # Calculer le prix de l'option comme la somme pondérée des prix des enfants (prix théorique)
-                option_value = discount * sum(child.option_price * prob for child, prob in current_node.children)
+            column = step 
+            current_row_df1 = self.N 
+            current_row_df2 = current_row_df3 = step 
+            current_node = trunc
 
-                if self.option_style == "american":
-                    # On calcul le payoff à cette date, et on prend le max entre le payoff et le prix théorique de l'option
-                    current_node.payoff(self.K, self.option_type)
-                    current_node.option_price = max(option_value, current_node.option_price)
-                else:  # Option européenne
-                    # Utiliser uniquement le prix théorique de l'option pour les options européennes
-                    current_node.option_price = option_value
-        
-        # Retourner le prix de l'option au nœud racine
-        return self.tree[0][0].option_price
+            while current_node is not None:
+                df1.iloc[current_row_df1, column] = current_node.value
+                df2.iloc[current_row_df2, column] = current_node.value
+                df3.iloc[current_row_df3, column] = current_node.value
 
-    def visualize_tree(self, style='ko-', threshold=0.0):
-        """
-        Plots the trinomial tree (lattice) for stock prices using matplotlib from a trinomial tree.
-        Values with cumulative probabilities below a threshold are covered in white.
+                current_row_df1 += 1
+                current_row_df2 += 1
+                current_row_df3 -= 1
 
-        Parameters
-        ----------
-        tree : list of lists
-            A list containing lists of nodes, representing the trinomial tree steps.
+                current_node = current_node.down_neighbor
+
+            current_row_df1 = self.N  
+            current_row_df2 = current_row_df3 = step 
+            current_node = trunc 
             
-        style : str
-                Matplotlib style for plotting lines and markers. Default is 'ko-'.
-        
-        threshold : float
-                    The threshold value for cumulative probability below which nodes are not plotted.
-        """
-        
+            while current_node is not None:
+                df1.iloc[current_row_df1, column] = current_node.value
+                df2.iloc[current_row_df2, column] = current_node.value
+                df3.iloc[current_row_df3, column] = current_node.value
+
+                current_row_df1 -= 1
+                current_row_df2 -= 1
+                current_row_df3 += 1
+                
+                current_node = current_node.up_neighbor
+
+            trunc = trunc.forward_mid_neighbor
+            
+        self.number_of_nodes = df1.count().sum()
+
+        return df1, df2, df3
+    
+    def visualize_tree(self, style='ko-'):
+
         # Calculate the number of steps in the tree
-        N = len(self.tree)
+        N = self.N
 
         # Calculate cumulative time steps for the x-axis based on self.deltaT_array
         time_steps = np.cumsum(np.insert(self.deltaT_array, 0, 0))
@@ -175,27 +177,8 @@ class TrinomialTreeModel(Market, Option):
 
         # Dynamically adjust figure size based on the number of steps
         plt.figure(figsize=(min(20, max(N * 0.4, 14)), min(10, max(N * 0.2, 7))))  # Width and height increase proportionally to N, but capped for readability
-        
-        # Dictionary to hold values for plotting
-        vals_down_node = {}
-        vals_up_node = {}
 
-        # Collect stock prices and cumulative probabilities from the trinomial tree nodes for each step
-        for i, step in enumerate(self.tree):
-            # Extract stock prices or values for the current step
-            vals_down_node[i] = [[node.value, node.cum_prob] for node in step]  # Use 'node.value' or 'node.stock_price' based on your node definition
-            vals_up_node[i] = list(reversed(vals_down_node[i]))  # For reverse plotting, we reverse the list
-        
-        # Filter values based on the threshold and convert collected data into Pandas DataFrames for easier plotting
-        dict_down = {k: pd.Series([np.nan if sublist[1] < threshold else sublist[0] for sublist in v]) for k, v in vals_down_node.items()}
-        dict_up = {k: pd.Series([np.nan if sublist[1] < threshold else sublist[0] for sublist in v]) for k, v in vals_up_node.items()}
-
-        # Create DataFrames for plotting
-        df_down = pd.DataFrame(dict_down)
-        df_up = pd.DataFrame(dict_up)
-        df_mid = df_up.copy()
-        for i in range(len(df_mid.columns)-1, -1, -1):
-            df_mid[i] = df_mid[i].shift(-(i - len(df_mid.columns) + 1))
+        df_mid, df_up, df_down = self.save_tree_to_dataframe()
         
         # Rename the columns for the mid DataFrame with time steps
         df_mid.columns = time_steps
@@ -214,12 +197,6 @@ class TrinomialTreeModel(Market, Option):
         plt.xlabel("Maturity (Cumulative)")
         plt.ylabel("Stock Price")
 
-        # Display the plot
         return plt
-    
-    def get_number_of_nodes(self):
-        """
-        Returns the total number of nodes in the trinomial tree.
-        """
-        return sum(1 for step in self.tree for node in step if node is not None)
-    
+
+
--- Classes/Node.py
+++ Classes/Node.py
@@ -1,19 +1,173 @@
-class Node:
-    def __init__(self, value, prob=0.0, cum_prob=0.0):
-        self.value = value  # Valeur de l'actif sous-jacent
-        self.prob = prob  # Probabilité de ce mouvement à partir du parent
-        self.cum_prob = cum_prob  # Probabilité cumulée d'atteindre ce nœud
-        self.option_price = 0.0  # Prix de l'option au nœud
-        self.children = []  # Liste des enfants du nœud
-        self.parent = None  # Parent du nœud
-    
-    def payoff(self, K, option_type):
-        if option_type == "call":
-            self.option_price = max(self.value - K, 0)
-        else:
-            self.option_price = max(K - self.value, 0)
+import math
+import numpy as np
 
-    #def calculate_prob(self, parent_prob):
+class Node:
+    def __init__(self, value, step, tree):
+        self.value = value
+        self.step = step
+
+        self.tree = tree
+        self.option_price = 0.0  # Initialisation explicite de l'attribut privé ici
+
+        self.cum_prob = 0.0
+
+        self.up_neighbor = None
+        self.down_neighbor = None
+        self.backward_neighbor = None
+
+        self.forward_up_neighbor = None
+        self.forward_mid_neighbor = None
+        self.forward_down_neighbor = None
+
+        self.prob_forward_up_neighbor = None
+        self.prob_forward_mid_neighbor = None
+        self.prob_forward_down_neighbor = None
+
+    def create_forward_nodes(self):
+
+        deltaT, alpha = self.get_deltaT_alpha()
+
+        self.forward_mid_neighbor = Node(self.value * math.exp(self.tree.rate * deltaT) - self.tree.dividend_value(self.step), self.step + 1, self.tree)
+        self.forward_mid_neighbor.backward_neighbor = self
+
+        self.forward_up_neighbor = Node(self.forward_mid_neighbor.value * alpha, self.step + 1, self.tree)
+        self.forward_mid_neighbor.up_neighbor = self.forward_up_neighbor
+        self.forward_mid_neighbor.up_neighbor.down_neighbor = self.forward_mid_neighbor
+        
+        self.forward_down_neighbor = Node(self.forward_mid_neighbor.value / alpha, self.step + 1, self.tree)
+        self.forward_mid_neighbor.down_neighbor = self.forward_down_neighbor
+        self.forward_mid_neighbor.down_neighbor.up_neighbor = self.forward_mid_neighbor
+
+        self.compute_probabilities()
+
+    def generate_upper_neighbors(self):
+
+        deltaT, alpha = self.get_deltaT_alpha()
+
+        trunc = self
+
+        actual_forward_up_node = self.forward_up_neighbor
+
+        while self.up_neighbor is not None:
+
+            actual_forward_up_node.up_neighbor = Node(actual_forward_up_node.value * alpha, self.step + 1, self.tree)
+            actual_forward_up_node.up_neighbor.down_neighbor = actual_forward_up_node
+            
+            supposed_mid_for_up_node_value = self.up_neighbor.value * math.exp(self.tree.rate * deltaT) - self.tree.dividend_value(self.step)
+
+            if supposed_mid_for_up_node_value < actual_forward_up_node.value * (1 + alpha) / 2 and supposed_mid_for_up_node_value > actual_forward_up_node.value * (1 + 1 / alpha) / 2:
+
+                self.up_neighbor.forward_down_neighbor = actual_forward_up_node.down_neighbor
+                self.up_neighbor.forward_mid_neighbor = actual_forward_up_node
+                self.up_neighbor.forward_up_neighbor = actual_forward_up_node.up_neighbor
+
+                self.up_neighbor.compute_probabilities()
+
+                if self.up_neighbor.forward_up_neighbor.cum_prob < self.tree.threshold:
+                    self.up_neighbor.forward_up_neighbor = None
+                    actual_forward_up_node.up_neighbor = None
+
+                self = self.up_neighbor
+
+            actual_forward_up_node = actual_forward_up_node.up_neighbor
+
+        self = trunc
+
+    def generate_lower_neighbors(self):
+
+        deltaT, alpha = self.get_deltaT_alpha()
+
+        trunc = self
+
+        actual_forward_down_node = self.forward_down_neighbor
+
+        while self.down_neighbor is not None:
+            
+            actual_forward_down_node.down_neighbor = Node(actual_forward_down_node.value / alpha, self.step + 1, self.tree)
+            actual_forward_down_node.down_neighbor.up_neighbor = actual_forward_down_node
+            
+            supposed_mid_for_down_node_value = self.down_neighbor.value * math.exp(self.tree.rate * deltaT) - self.tree.dividend_value(self.step)
+
+            if supposed_mid_for_down_node_value <= actual_forward_down_node.value * (1 + alpha) / 2 and supposed_mid_for_down_node_value >= actual_forward_down_node.value * (1 + 1 / alpha) / 2:
+                
+                self.down_neighbor.forward_down_neighbor = actual_forward_down_node.down_neighbor
+                self.down_neighbor.forward_mid_neighbor = actual_forward_down_node
+                self.down_neighbor.forward_up_neighbor = actual_forward_down_node.up_neighbor
+                
+                self.down_neighbor.compute_probabilities()
+                
+                if self.down_neighbor.forward_down_neighbor.cum_prob < self.tree.threshold:
+                    self.down_neighbor.forward_down_neighbor = None
+                    actual_forward_down_node.down_neighbor = None
+
+                self = self.down_neighbor
+
+            actual_forward_down_node = actual_forward_down_node.down_neighbor
+
+        self = trunc
+
+    def compute_option_price(self):
+
+        discount = np.exp(-self.tree.rate * self.tree.deltaT_array[self.step])
+
+        option_value = 0.0
+
+        if self.forward_up_neighbor is not None:
+            value = self.forward_up_neighbor.option_price
+            option_value += value * self.prob_forward_up_neighbor
+
+        if self.forward_mid_neighbor is not None:
+            value = self.forward_mid_neighbor.option_price
+            option_value += value * self.prob_forward_mid_neighbor
+
+        if self.forward_down_neighbor is not None:
+            value = self.forward_down_neighbor.option_price
+            option_value += value * self.prob_forward_down_neighbor
+
+        option_value *= discount
+
+        if self.tree.option_style == "american":
+            self.payoff()
+            self.option_price = max(option_value, self.option_price)
+        else:
+            self.option_price = option_value
+        
+    def payoff(self):
+
+        if self.tree.option_type == "call":
+            self.option_price = max(self.value - self.tree.K, 0)
+        else:
+            self.option_price = max(self.tree.K - self.value, 0)
+
+    def compute_probabilities(self):
+
+        deltaT, alpha = self.get_deltaT_alpha()
+
+        esp = self.value * math.exp(self.tree.rate * deltaT) - self.tree.dividend_value(self.step)
+        var = self.value ** (2) * math.exp(2 * self.tree.rate * deltaT) * (math.exp(self.tree.sigma ** 2 * deltaT) - 1)
+
+        down_prob = (self.forward_mid_neighbor.value ** (-2) * (var + esp ** 2) - 1 - (alpha + 1) * (esp / self.forward_mid_neighbor.value - 1)) / ((1 - alpha) * (alpha ** (-2) - 1))
+        up_prob = (esp / self.forward_mid_neighbor.value - 1 - (1 / alpha - 1) * down_prob) / (alpha - 1)
+        mid_prob = 1 - up_prob - down_prob
+
+        self.prob_forward_down_neighbor = down_prob
+        self.prob_forward_up_neighbor = up_prob
+        self.prob_forward_mid_neighbor = mid_prob
+
+        if self.forward_up_neighbor is not None:
+            self.forward_up_neighbor.cum_prob += self.cum_prob * self.prob_forward_up_neighbor
+
+        if self.forward_mid_neighbor is not None:
+            self.forward_mid_neighbor.cum_prob += self.cum_prob * self.prob_forward_mid_neighbor
+
+        if self.forward_down_neighbor is not None:
+            self.forward_down_neighbor.cum_prob += self.cum_prob * self.prob_forward_down_neighbor
+
+    def get_deltaT_alpha(self):
+
+        deltaT = self.tree.deltaT_array[self.step]
+        alpha = math.exp(self.tree.sigma * math.sqrt(3 * deltaT))
+        return deltaT, alpha
 
     def __repr__(self):
-        return f"Node(value={self.value:.2f}, option_price={self.option_price:.5f}, prob={self.prob:.5f}, cum_prob={self.cum_prob:.5f})"
+        return f"value={self.value:.2f}, step={self.step}, option_price={self.option_price:.2f}, cum_prob={self.cum_prob:.5f}"
